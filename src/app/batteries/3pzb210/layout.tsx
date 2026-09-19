407fc0937fb05d2f9165d5921c7ecf9f5a14a5e2
gokul_manokaran <alexgokul350@gmail.com>
2026-09-19 18:21:31 +0530

    done

--- src/app/batteries/3pzb210/layout.tsx
+++ src/app/batteries/3pzb210/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
     url: "https://www.anfaasarabia.com/batteries/3pzb210",
   },
 };
+//
 
 export default function Battery3PzB210Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
